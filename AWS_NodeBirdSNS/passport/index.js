3a900531f3da2f7e5fdfe2cf9e40c6887b05b107
ByeongJun <neostgeart@ajou.ac.kr>
2021-07-19 02:12:21 +0900

    Initial commit

---
+++ AWS_NodeBirdSNS/passport/index.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const User = require('../models/user');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findOne({
+      where: { id },
+      include: [{
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followers',
+      }, {
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followings',
+      }],
+    })
+      .then(user => done(null, user)) //이 이후에 req.user, req.isAuthenticated() 사용 가능.
+      .catch(err => done(err));
+  });
+
+  local();
+  kakao();
+};
